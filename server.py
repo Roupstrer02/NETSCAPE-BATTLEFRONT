f56f03f76e1de3d69e9b137df0c5b4d9fbdbcc32
R-kaloustian <102569106+Roupstrer02@users.noreply.github.com>
2024-11-09 14:27:49 -0500

    Basic server side code completed

--- server.py
+++ server.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template, request
 
-
+#===================================================================================================
+# Flask server for student interaction of PyGame Project
+#
+# Roupen Kaloustian
+# November 9th, 2024
+#===================================================================================================
+# To-do List
+#   - Add js script that checks the student_input.txt file's 2 first rows to check if certain form options are legal to press
+#   - add an error message window for any important messages that need to get sent to players
+#
+#===================================================================================================
 def activate_server():
     print("Starting Game Server...")
 
@@ -15,15 +25,20 @@ def activate_server():
     def user_input():
         
         unit_type = request.form["unit_type"]
-        #unit_count = request.form["unit_count"]
-        #unit_starting_location = request.form["starting_location"]
-        #unit_destination = request.form["destination"]
+        unit_count = request.form["unit_count"]
+        unit_starting_locations = request.form["unit_spawn"]
+        unit_destination = request.form["unit_destination"]
+        
+
 
         with open('student_input.txt', 'a') as f:
-            f.write(unit_type + '\n')
+            f.write(unit_type + " " + unit_starting_locations + " " + unit_destination + " " + unit_count + '\n')
 
+            
         
 
         return render_template('index.html')
 
     app.run(host="0.0.0.0")
+
+activate_server()
